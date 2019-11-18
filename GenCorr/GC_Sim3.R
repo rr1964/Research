f853007b530b79fc33bf9f1c9f170ceddd1b40ba
rr1964 <rreese531@gmail.com>
2019-11-18 14:09:48 -0700

    Update GC_Sim3.R

--- GenCorr/GC_Sim3.R
+++ GenCorr/GC_Sim3.R
@@ -4,8 +4,8 @@
 #Sim 3
 
 
-source("/uufs/chpc.utah.edu/common/home/u6011224/GenCorr/GenCorrFunct.R")
-#source("GenCorr Functions.R")
+#
+source("GenCorr Functions.R")
 #print("pig")
 
 library(data.table)
@@ -136,7 +136,7 @@ for(r in 1:100)
   
 }
 
-fileName = paste0("/uufs/chpc.utah.edu/common/home/u6011224/GenCorr/", "Sim3Results_", arg[[1]],".csv")
+fileName = paste0("PATH", "Sim3Results_", arg[[1]],".csv")##Put in your own path
 
 fwrite(as.data.frame(score), 
        file = fileName,
